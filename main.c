b8bffeb87110b9ce0518aea3d9ac99871c0439a9
xiang123abc <36296072+xiang123abc@users.noreply.github.com>
2022-04-26 09:42:15 +0800

    Update main.c
    aa

--- main.c
+++ main.c
@@ -2,9 +2,47 @@
 
 
 int fun(int a,int b){
+    int x;
+    int y;
+    int abce;
+    int agege=3;
+    char cdge=  "";
+    while(1){
+
+    }
 
     return  a+b;
 }
+int fun2(int a,int b){
+    int x;
+    int y;
+    int abce;
+    int agege=3;
+    char cdge=  "";
+    while(1){
+
+    }
+
+    return  a+b;
+}
+
+int fun3(int a,int b){
+    int x;
+    int y;
+    int abce;
+    int agege=3;
+    char cdge=  "";
+    while(1){
+
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        
+    }
+    return  a+b;
+}
+
+
 
 int main() {
     printf("Hello, World!\n");
